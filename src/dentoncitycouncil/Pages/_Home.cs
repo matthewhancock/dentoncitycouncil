abcc664b583d0e5f3a485bfdd53b78eaf2a0d47f
Matthew Hancock <git@mt.hn>
2016-01-26 18:05:05 -0500

    Create _Home.cs
    post election changes

---
+++ src/dentoncitycouncil/Pages/_Home.cs
@@ -0,0 +1,70 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dentoncitycouncil.Pages {
+    public class Home : Site.Page {
+
+        public static string GenerateContent() {
+            var sb = new StringBuilder();
+            var links = JsonConvert.DeserializeObject<List<Site.Link>>(Util.File.LoadToString("Links/Home.json").Result);
+            var content = Util.File.LoadToString("Content/Home.txt").Result;
+            
+            sb.Append(content.Replace("\r\n", "<br />"));
+            sb.Append("<br /><br />" +  Site.Svg.Signature + "<img src=\"/image/Pope-Ayatollah\" />");
+            //sb.Append("<hr />The links below are to relevant Portsmouth Herald articles and letters to the editor I authored:<br /><br />");
+            //foreach (var i in links) {
+            //    sb.Append($"<a href=\"{i.Url}\" target=\"_blank\">{i.Title}</a><br />{i.Date.ToString("MMMM dd, yyyy")}");
+            //    if (i.Description != null) {
+            //        sb.Append("<br />" + i.Description);
+            //    }
+            //    sb.Append("<br /><br />");
+            //}
+            return sb.ToString();
+        }
+
+        private string _content;
+        public override string Content {
+            get {
+                if (_content == null) {
+                    _content = GenerateContent();
+                }
+                return _content;
+            }
+        }
+
+        public override string Description {
+            get {
+                return "";
+            }
+        }
+
+        public override string Key {
+            get {
+                return "home";
+            }
+        }
+        public override string Path {
+            get {
+                return string.Empty;
+            }
+        }
+        public override string Header {
+            get {
+                return "Home";
+            }
+        }
+
+        public override string Title {
+            get {
+                return Application.Title;
+            }
+        }
+
+        public override string TitleNav {
+            get {
+                return "Home";
+            }
+        }
+    }
+}
